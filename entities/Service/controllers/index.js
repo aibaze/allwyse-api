1c66715ba407b7b9815784855ada1c578d2283ad
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2023-12-29 02:42:55 +0800

    serviceDeletion done

--- entities/Service/controllers/index.js
+++ entities/Service/controllers/index.js
@@ -23,11 +23,36 @@ const createService = async (req, res) => {
   }
 };
 
+const deleteService = async (req, res) => {
+  try {
+    const { coachId, serviceId } = req.params;
+    await Service.deleteOne({ _id: new ObjectId(serviceId) });
+
+    await Coach.updateOne(
+      {
+        _id: new ObjectId(coachId),
+        services: { $in: [new ObjectId(serviceId)] },
+      },
+      {
+        $pull: {
+          services: new ObjectId(serviceId),
+        },
+      }
+    );
+
+    res.status(200).json({ message: "Delete successfull" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const getServicesByCoachId = async (req, res) => {
   try {
     const { coachId } = req.params;
 
-    const coachServices = await Coach.findOne({ _id: new ObjectId(coachId) }).lean() || []
+    const coachServices =
+      (await Coach.findOne({ _id: new ObjectId(coachId) }).lean()) || [];
     const services = await Service.find({
       _id: { $in: coachServices.services },
     });
@@ -39,4 +64,4 @@ const getServicesByCoachId = async (req, res) => {
   }
 };
 
-module.exports = { createService ,getServicesByCoachId};
+module.exports = { createService, getServicesByCoachId,deleteService };
--- entities/Service/routes/index.js
+++ entities/Service/routes/index.js
@@ -1,9 +1,10 @@
 const express = require("express") ;
-const {createService,getServicesByCoachId} = require("../controllers")
+const {createService,getServicesByCoachId,deleteService} = require("../controllers")
 const serviceRouter = express.Router();
 
 //ROUTES
 serviceRouter.post("/create-service/coach/:coachId", createService);
 serviceRouter.get("/coach/:coachId", getServicesByCoachId);
+serviceRouter.delete("/:serviceId/coach/:coachId", deleteService);
 
 module.exports = { serviceRouter };
