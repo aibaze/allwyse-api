9c0b4574cb87fed2369c0729b13b2ee1dda2c0c0
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2024-01-17 04:17:01 +0800

    securized

--- entities/Coach/controllers/index.js
+++ entities/Coach/controllers/index.js
@@ -1,27 +1,6 @@
 const { ObjectId } = require("mongodb");
 const Coach = require("../../../models/Coach");
-/* const jsonwebtoken = require("jsonwebtoken");
-const jwkToPem = require("jwk-to-pem");
-const jsonWebKeys = {
-  keys: [
-    {
-      alg: "RS256",
-      e: "AQAB",
-      kid: "QIb3NkaNlydPkO8iRyybwL24bH95dKFXXA2Bx4fe9Ak=",
-      kty: "RSA",
-      n: "_a3AiL47VIgVgx6z3cfQHKanjqForDfH6d5UIQjNPwpCe7Zm-DGWchjKE8m73NcbOxIgUTfuMFPphRqTVT8HNOrzl5CEKDx6RHJDLBQRLJyKvLoXaXdY5u1oDZGU6RA0njhEGHhUVn4MJXiN-cP4MDOZguIf17YghuWO6DwuXbdwDPNJQr0ia8Vj3nIOKlhjTP5-bSxHMBkZZeWIV8zBGWdZQPNt-JOwhct81W5eYRAaXfVSZj4yguIBz4cCfASdRfSB7A3mZY9CM0O4SEIFk1Mt6o1XEHxrmK-bGAPNAgiJjzLx0U3pU1liha1EHq0EUmSeRk1PzYXCqT0najvAZw",
-      use: "sig",
-    },
-    {
-      alg: "RS256",
-      e: "AQAB",
-      kid: "d66WtUXZudiUYOfakjgYZP3W44iiVGJZBqQGhilBWpE=",
-      kty: "RSA",
-      n: "q8uBORp-x2khYaAbPsFIjw577c2K3gMECao8sZUmvB_3S8CyR7MJBLPgUoogrcibcs_3wXIhNF67Q2byKtBTn8qhAEetq-cDvEpHbPsDsvLQwzYK4sTAERKpoo1FdHr0cav-lk2nl8RIHx2o9Mp1fp02IFhMylOuSL-wTjMKtVwx9c6Ij5EKoUkVxLh2Oz_kXi4BSoeUOEpcRMUmF7IvfervkJHHcpIOhYwYQ22y6P1Ii_E2xHdGcQMhSX2RnXMLzlKzsqRXfKSg8udMRpKl9zyHAroHEfQkUoFWsPdszCYUQRQ-7F5xG1woJVlNsbx_xK6KZJ3xr-z0wu-eOs1yEw",
-      use: "sig",
-    },
-  ],
-}; */
+
 
 const createSlug = (firstName, lastName) => {
   const rawSlug = `${firstName?.toLowerCase()}-${lastName?.toLowerCase()}`;
