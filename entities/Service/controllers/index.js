f859e3757d43bfa2202eeda118c5f4faacd32371
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2024-05-31 14:43:01 -0300

    service model update

--- entities/Service/controllers/index.js
+++ entities/Service/controllers/index.js
@@ -5,7 +5,6 @@ const { ObjectId } = require("mongodb");
 const createService = async (req, res) => {
   try {
     const { coachId } = req.params;
-    const newService = await Service.create({ ...req.body, coachId });
 
     await Coach.updateOne(
       { _id: new ObjectId(coachId) },
