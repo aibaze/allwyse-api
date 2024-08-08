2bf054ad777ef216b2dd7004d1eb4b589c10c048
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2024-08-08 01:05:15 -0300

    improve google auth

--- models/GoogleInfo.js
+++ models/GoogleInfo.js
@@ -4,10 +4,13 @@ const GoogleInfoSchema = mongoose.Schema({
   token: {
     type: String,
   },
+  access_token: {
+    type: String,
+  },
   expiresIn: { type: Number },
   coachId: { type: mongoose.Types.ObjectId },
 });
 
-const GoogleInfo = mongoose.model("GoogleInfo", GoogleInfoSchema,"googleInfo");
+const GoogleInfo = mongoose.model("GoogleInfo", GoogleInfoSchema, "googleInfo");
 
 module.exports = { GoogleInfoSchema, GoogleInfo };
