de74c764af8e4b775b3589e19fd97520bd782455
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2024-08-15 11:27:23 -0300

    updated model

---
+++ models/Experience.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const ExperienceSchema = mongoose.Schema({
+  area: {
+    type: String,
+  },
+  brand: {
+    type: String,
+  },
+  year: {
+    type: String,
+  },
+});
+
+module.exports = { ExperienceSchema };
