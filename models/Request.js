38990f9feff8c290d90335916304dde08243c4fb
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2024-12-26 04:27:09 +0400

    add open ai handlers improvments

--- models/Request.js
+++ models/Request.js
@@ -56,6 +56,9 @@ const RequestSchema = mongoose.Schema(
     completionStage: {
       type: String,
     },
+    questionary: {
+      type: [{ question: String, answer: String }],
+    },
   },
   {
     timestamps: true,
