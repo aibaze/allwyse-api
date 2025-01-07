d79ce121f285d951714ebaa6917ad21b6280b480
Tomas Goldenberg <soytomasgoldenberg@gmail.com>
2025-01-07 23:28:09 +0400

    verifyMiddleware

--- entities/Event/routes/index.js
+++ entities/Event/routes/index.js
@@ -8,11 +8,12 @@ const {
 } = require("../controllers");
 const eventRouter = express.Router();
 const authMiddleware = require("../../../middlewares/AuthMiddleware");
+const ownershipMiddleware = require("../../../middlewares/OwnershipMiddleware");
 
 // ROUTES
 eventRouter.post("/create-event", authMiddleware, createEvent);
-eventRouter.get("/coach/:coachId", authMiddleware, getEventsByCoach);
-eventRouter.get("/google/authorized/:coachId", authMiddleware, checkIfItsAuth);
+eventRouter.get("/coach/:coachId", authMiddleware,ownershipMiddleware, getEventsByCoach);
+eventRouter.get("/google/authorized/:coachId", authMiddleware,ownershipMiddleware, checkIfItsAuth);
 eventRouter.post("/google/auth", authMiddleware, googleAuth);
 
 // PUBLIC ROUTES
