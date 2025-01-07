bd5371ebb018de4bf712e5ebebd6a896432ac333
Tomas Goldenberg <ext.tgoldenberg@riotgames.com>
2025-01-08 00:03:04 +0400

    done

--- middlewares/OwnershipMiddleware.js
+++ middlewares/OwnershipMiddleware.js
@@ -1,28 +1,27 @@
 const Coach = require("../models/Coach");
-const {notifyError} = require("../utils/error");
+const { notifyError } = require("../utils/error");
 
-const OwnershipVerification =async (req, res, next) => {
+const OwnershipVerification = async (req, res, next) => {
+  try {
+    const email = req.loggedUser.email || req.loggedUser;
+    const coachId = req.params.id || req.params.coachId;
+    const currentCoach = await Coach.findOne({ email });
 
-    try {
-        const email = req.loggedUser.email || req.loggedUser;
-        const coachId = req.params.id || req.params.coachId;
-        const currentCoach = await Coach.findOne({email})
-      
-        if(!currentCoach || !coachId){
-          throw new Error("Coach not found")
-        }
-      
-        if(currentCoach._id?.toString() !== coachId?.toString()){
-          notifyError(new Error(`${email} is not the owner of this resource`));
-          throw new Error("Forbidden")
-        }
-
-        next();
-
-    } catch (error) {
-        return res.status(403).json({ error: true, message: "Forbidden" });
-        
+    if (!currentCoach || !coachId) {
+      throw new Error("Coach not found");
     }
-  };
-  module.exports = OwnershipVerification;
-  
+
+    const matchId = currentCoach._id.toString() !== coachId?.toString();
+    const matchEmail = currentCoach.email.toString() !== coachId?.toString();
+
+    if (matchId && matchEmail) {
+      notifyError(new Error(`${email} is not the owner of this resource`));
+      throw new Error("Forbidden");
+    }
+
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: true, message: "Forbidden" });
+  }
+};
+module.exports = OwnershipVerification;
