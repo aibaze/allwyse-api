e08d377055e20bf7afa583f9c1c168dddff35a83
Tomas Goldenberg <soytomasgoldenberg@gmail.com>
2025-02-15 20:38:33 +0700

    done

--- entities/Utils/routes.js
+++ entities/Utils/routes.js
@@ -9,5 +9,9 @@ const {
 // PRIVATE ROUTES
 utilRouter.post("/send-email", authMiddleware, sendEmailController);
 utilRouter.post("/execute-prompt", authMiddleware, executePromptController);
+utilRouter.post("/vapi/end-of-call", (req,res)=>{
+console.log(req.body,"BODY")
+console.log(req.headers,"HEADERS")
+});
 
 module.exports = { utilRouter };
