f6228a4c1df444c30fe0f616e1bcf8fc89e48348
Tomas Goldenberg <soytomasgoldenberg@gmail.com>
2025-02-17 18:20:29 +0700

    add get calls endpoint

--- entities/Utils/routes.js
+++ entities/Utils/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const authMiddleware = require("../../middlewares/AuthMiddleware");
 const utilRouter = express.Router();
 const {
@@ -11,7 +12,14 @@ utilRouter.post("/send-email", authMiddleware, sendEmailController);
 utilRouter.post("/execute-prompt", authMiddleware, executePromptController);
 utilRouter.post("/vapi/end-of-call", (req,res)=>{
 console.log(req.body,"BODY")
-console.log(req.headers,"HEADERS")
+});
+utilRouter.get("/vapi/calls",async (req,res)=>{
+  const {data} = await axios.get("https://api.vapi.ai/call",{
+    headers: {
+      'Authorization': `Bearer ${process.env.VAPI_KEY}`
+    }
+  })
+  res.status(200).json(data)
 });
 
 module.exports = { utilRouter };
